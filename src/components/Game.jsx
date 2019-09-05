b30d9cf39b938504cc50f33c7dcd0adcd00bad7d
Julie <sizumasa@hotmail.com>
2019-09-05 15:44:18 +0800

    move game logic to Game, keep Tiles and Tile pure component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -10,14 +10,45 @@ class Game extends Component {
       name: '',
       step: 0,
     },
-    box: {
+    boxes: {
       row: 3,
       col: 3,
+      size: 0,
+      list: [],
+      matrix: []
     },
   };
 
+  componentDidMount() {
+    this.getBoxes(this.state.boxes.row, this.state.boxes.col);
+  }
+
+  getBoxes(row, col) {
+    const size = row * col;
+    const list = Array.from({length: size}, (v, i) => i+1);
+    
+    let oneD = [...list];
+    let twoD = [];
+    row = row > 0 ? row : 0;
+    while(row-- && oneD.length > 0) {
+      let chunk = oneD.splice(0,col)
+      twoD.push(chunk);
+    }
+
+    let newBoxes = {...this.state.boxes};
+    newBoxes.size = size;
+    newBoxes.list = list;
+    newBoxes.matrix = twoD;
+
+    this.setState({ boxes: newBoxes });
+  }
+
+  onTileClick () {
+    console.log('Tile Clicked!')
+  }
+
   render() {
-    const { player, box } = this.state;
+    const { player, boxes } = this.state;
     return (
       <div className="Game">
         <div className="Game-info">
@@ -25,7 +56,10 @@ class Game extends Component {
           <Info />
           <p>名字、步數 {player.name + player.step}</p>
         </div>
-        <Tiles row={box.row} col={box.col} />
+        <Tiles
+          list={boxes.list}
+          onTileClick={this.onTileClick}
+        />
       </div>
     );
   }
--- src/components/Tiles.jsx
+++ src/components/Tiles.jsx
@@ -1,54 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Tile from './Tile';
 
-class Tiles extends Component {
-  state = {
-    boxes: Array.from({length: this.props.row * this.props.col}, (v, i) => i+1),
-    matrix: [],
-  };
-
-  componentDidMount() {
-    this.getMatrix(this.state.boxes, this.props.row, this.props.col);
-  }
-
-  componentDidUpdate() {
-    // 如果 boxes 洗牌或換位，Ｍatrix 重新計算
-  }
-
-  getMatrix = (boxes, row, col) => {
-    let oneD = [...boxes];
-    row = row > 0 ? row : 0;
-    let twoD = [];
-    while(row-- && oneD.length > 0) {
-      let chunk = oneD.splice(0,col)
-      twoD.push(chunk);
-    }
-    this.setState({ matrix: twoD });
-  }
-
-  onTileClick = () => {
-    console.log('Tile Clicked!');
-    // 檢查可否移動，可就移然後更新 matrix，不可就沒事
-  };
-
-  render() {
-    const { boxes } = this.state;
-    return (
-      <div>
-        {boxes.map((m, i) => {
-          let text = i+1;
-          return <Tile key={i} text={text.toString()} onTileClick={this.onTileClick} />;
-        })}
-      </div>
-    );
-  }
-}
+const Tiles = ({ list, onTileClick }) => (
+  <div>
+    {list.map((m, i) => {
+      let text = i+1;
+      return <Tile key={i} text={text.toString()} onTileClick={onTileClick} />;
+    })}
+  </div>
+);
 
 Tiles.propTypes = {
-  row: PropTypes.number.isRequired,
-  col: PropTypes.number.isRequired,
+  list: PropTypes.array.isRequired,
+  onTileClick: PropTypes.func.isRequired,
 };
 
 export default Tiles;
