3db2b0e3f400517b3196be58a3597deef88e09fc
Julie <sizumasa@hotmail.com>
2019-09-22 02:53:48 +0800

    check if clicked tile can swap done

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -48,27 +48,71 @@ class Game extends Component {
     }
   }
 
-  clickTile = () => {
-    const { isStart } = this.state;
+  findCoord = val => {
+    const { boxesMatrix } = this.state;
+    for (let i = 0; i < boxesMatrix.length; i += 1) {
+      for (let j = 0; j < boxesMatrix.length; j += 1) {
+        if (boxesMatrix[i][j] === parseInt(val, 10)) {
+          return { x: i, y: j };
+        }
+      }
+    }
+    return -1;
+  };
+
+  canSwap = tile => {
+    const tileX = this.findCoord(tile).x;
+    const tileY = this.findCoord(tile).y;
+    const holeX = this.findCoord(9).x;
+    const holeY = this.findCoord(9).y;
+    return Math.abs(tileX - holeX) + Math.abs(tileY - holeY) === 1;
+  };
+
+  calcDistance = ({ x, y }) => {
+    return {
+      xMove: x * 100,
+      yMove: y * 100,
+    };
+  };
+
+  clickTile = e => {
+    const { isStart, boxesList } = this.state;
     if (isStart) {
-      // step ++
-      this.setState(prevState => {
-        return { playerStep: `${parseInt(prevState.playerStep, 10) + 1}` };
-      });
+      const val = e.currentTarget.value;
       // 檢查是否和挖空相鄰（用矩陣），相鄰代表可移動
-      // 如可移動，就 Target 跟 挖空 換位
-      // cue 換位動畫（動畫時其他地方鎖起來以防 User 連續亂按）
+      if (this.canSwap(val)) {
+        // 如可移動，就 Target 跟 挖空 換位
+        // cue 換位動畫（動畫時其他地方鎖起來以防 User 連續亂按）
+        console.log('Swap!!!');
+        // let swapedList = [...boxesList];
+        // swapedList[val]
+        // this.setState({
+        // boxesList: swapedList;
+        // })
+        // step ++
+        this.setState(prevState => {
+          return { playerStep: `${parseInt(prevState.playerStep, 10) + 1}` };
+        });
+      }
       // 不可移動，就何も起こらない
+      console.log('Cannot swap!!!');
       // 檢查是否破關
+      this.checkSolved();
       console.log('Tile Clicked!');
     }
   };
 
-  checkSolved = e => {
+  checkSolved = () => {
+    const { boxesList } = this.state;
+    const ideal = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    if (boxesList.join() === ideal.join()) {
+      console.log('Solved!!!');
+      this.setState({ isStart: false });
+    }
     // 如果破關，cue 破關動畫（動畫時其他地方鎖起來以防 User 連續亂按）
     // isStart 變回 false，ReStart 變回 Start
     // 沒破關，何も起こらない
-    console.log('Check Solved', e);
+    console.log('Not solved!');
   };
 
   clickStart = () => {
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Tile = ({ text, clickTile }) => (
-  <button type='button' className='tile' onClick={clickTile}>
-    <p>{text}</p>
+  <button type='button' className='tile' onClick={clickTile} value={text}>
+    {text === '9' ? <p className='hole'>{text}</p> : <p>{text}</p>}
   </button>
 );
 
